7e56dd66e4e08ff7a54c47676c1361116a40dc4c
Ayazzali1 <ayazzali1@mail.ru>
2018-11-06 04:25:43 +0300

    possible without tg logs

--- convert_audio_message_to_text__bot/Services/TgLog.cs
+++ convert_audio_message_to_text__bot/Services/TgLog.cs
@@ -9,23 +9,30 @@ namespace convert_audio_message_to_text__bot.Services
     {
         TelegramBotClient bot { get; set; }
         string logBackTgId = "";
-        public TgLog(TelegramProvider bp, Settings s) { bot = bp.bot; logBackTgId = s.cfg["logBack"]; }
+        public TgLog(TelegramProvider bp, Settings s)
+        {
+            bot = bp.bot;
+            logBackTgId = s.cfg["logBack"];
+            if (string.IsNullOrWhiteSpace(logBackTgId))
+                Console.WriteLine("WARN: logs will not be sent to your telegram chat");
+        }
 
         int lBackMessageId = 0;
         public void l(string s)
         {
-            try
-            {
-                Console.WriteLine(s);
-                if (lBackMessageId == 0)
-                    lBackMessageId = bot.SendTextMessageAsync(logBackTgId, s).Result.MessageId;
+            Console.WriteLine(s);
+            if (!string.IsNullOrWhiteSpace(logBackTgId))
+                try
+                {
+                    if (lBackMessageId == 0)
+                        lBackMessageId = bot.SendTextMessageAsync(logBackTgId, s).Result.MessageId;
 
-                bot.EditMessageTextAsync(logBackTgId, lBackMessageId, s);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+                    bot.EditMessageTextAsync(logBackTgId, lBackMessageId, s);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
         }
     }
 }
